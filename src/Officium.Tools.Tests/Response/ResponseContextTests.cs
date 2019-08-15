ac27e77e6ed1578c0432657712752c45160e60b2
Simon Smith <55135068@ad.mmu.ac.uk>
2019-08-15 10:47:07 +0100

    ss - added tests for response context

---
+++ src/Officium.Tools.Tests/Response/ResponseContextTests.cs
@@ -0,0 +1,47 @@
+﻿using Officium.Tools.Response;
+using System;
+using System.Collections.Generic;
+using FluentAssert;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Officium.Tools.Tests.Response
+{
+    public class ResponseContextTests
+    {
+        [Fact]
+        public void CanBeConstructed()
+        {
+            new ResponseContent().ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ValidationErrorAreNotNull()
+        {
+            new ResponseContent().ValidationErrors.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ExceptionsCauseBadRequests()
+        {
+            new ResponseContent { Exception = new Exception() }.GetActionResult().ShouldBeOfType<BadRequestObjectResult>();
+        }
+
+
+        [Fact]
+        public void ValidationCauseBadRequests()
+        {
+            var ctx = new ResponseContent();
+            ctx.ValidationErrors.Add(new ValidationError("", ""));
+            ctx.GetActionResult().ShouldBeOfType<BadRequestObjectResult>();
+        }
+
+        [Fact]
+        public void DefaultsToOKResult()
+        {
+            var ctx = new ResponseContent();
+            ctx.GetActionResult().ShouldBeOfType<OkObjectResult>();
+        }        
+
+    }
+}
--- src/Officium.Tools/Response/ResponseContent.cs
+++ src/Officium.Tools/Response/ResponseContent.cs
@@ -7,19 +7,19 @@ namespace Officium.Tools.Response
 {
     public class ResponseContent
     {
-        internal ResponseContent()
+        public ResponseContent()
         {
             Result = new { };
         }
-        public readonly List<ValidationError> ValidationError = new List<ValidationError>();
+        public readonly List<ValidationError> ValidationErrors = new List<ValidationError>();
         public int StatusCode { get; set; }
         public object Result { get; set; }
         public Exception Exception { get; set; }
         public ActionResult GetActionResult()
         {
-            if (Exception != null)return new BadRequestObjectResult(Exception.Message);
-            if (ValidationError != null && ValidationError.Any())return new BadRequestObjectResult(ValidationError);
-            return new OkObjectResult(Result){ StatusCode = StatusCode};         
+            if (Exception != null) return new BadRequestObjectResult(Exception.Message);
+            if (ValidationErrors != null && ValidationErrors.Any())return new BadRequestObjectResult(ValidationErrors);
+            return new OkObjectResult(Result){StatusCode = StatusCode};         
         }
     }
 }
